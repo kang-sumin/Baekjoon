1f5b9d9554b472cf9fd671794732f18535b7af5f
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-07-01 21:37:21 +0900

    Create 10845.cpp
    baekjoon problem 10866 code

---
+++ C++/10845/10845.cpp
@@ -0,0 +1,67 @@
+/*
+* 문제번호 : 10845
+* 
+* https://www.acmicpc.net/problem/10845
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+queue <int> Q;
+
+int main() {
+
+	int N; //명령의 수를 저장할 변수 N
+	cin >> N;
+
+	//명령어 분석해서 출력함
+	for (int i = 0; i < N; i++) {
+		//명령어 str로 받아서 걸러냄
+		string str;
+		cin >> str;
+
+		//push : 정수x를 큐에 넣음
+		if (str == "push") {
+			int x;
+			cin >> x;
+			Q.push(x);
+		}
+		//pop : 가장 앞 정수를 빼고, 그 수 출력, 아무것도 없으면 -1 출력
+		else if (str == "pop") {
+			if (Q.empty()) {
+				cout << "-1" << endl;
+			}
+			else {
+				cout << Q.front() << endl;
+				Q.pop();
+			}
+		}
+		//큐가 비었으면 1, 아니면 0 출력
+		else if (str == "empty") {
+			cout << Q.empty() << endl;
+		}
+		//가장 앞 정수 출력하고, 아무것도 없으면 -1 출력
+		else if (str == "front") {
+			if (Q.empty()) {
+				cout << "-1" << endl;
+			}
+			else {
+				cout << Q.front() << endl;
+			}
+		}
+		//가장 뒤 정수 출력하고, 아무것도 없으면 -1 출력
+		else if (str == "back") {
+			if (Q.empty()) {
+				cout << "-1" << endl;
+			}
+			else {
+				cout << Q.back() << endl;
+			}
+		}
+		//큐에 들어가 있는 정수 개수 출력
+		else {
+			cout << Q.size() << endl;
+		}
+	}
+	return 0;
+}
